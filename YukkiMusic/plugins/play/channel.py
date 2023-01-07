82b57ff9c8d773a9a9a110c631b42f819b9407f2
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-01-07 16:24:28 +0300

    Update channel.py

--- YukkiMusic/plugins/play/channel.py
+++ YukkiMusic/plugins/play/channel.py
@@ -12,6 +12,7 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
+from strings.filters import command
 from YukkiMusic import app
 from YukkiMusic.utils.database import set_cmode
 from YukkiMusic.utils.decorators.admins import AdminActual
