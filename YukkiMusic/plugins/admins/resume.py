d9836750924ebf8f713229911f7ec9764e9f4945
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-01-07 03:37:45 +0300

    Update resume.py

--- YukkiMusic/plugins/admins/resume.py
+++ YukkiMusic/plugins/admins/resume.py
@@ -27,6 +27,12 @@ RESUME_COMMAND = get_command("RESUME_COMMAND")
     & ~filters.edited
     & ~BANNED_USERS
 )
+@app.on_message(
+    command(["استئناف","اكمل"])
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
