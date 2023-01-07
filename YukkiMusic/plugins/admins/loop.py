19d3b9bea5cf260d1b29dfaa77a749be36a6f424
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-01-07 16:12:24 +0300

    Update loop.py

--- YukkiMusic/plugins/admins/loop.py
+++ YukkiMusic/plugins/admins/loop.py
@@ -28,6 +28,12 @@ LOOP_COMMAND = get_command("LOOP_COMMAND")
     & ~filters.edited
     & ~BANNED_USERS
 )
+@app.on_message(
+    command(["كرر"])
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
 @AdminRightsCheck
 async def admins(cli, message: Message, _, chat_id):
     usage = _["admin_24"]
