e1ebda947258f2f88c5d0b7bdcd5b6e46c617645
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-01-07 03:10:15 +0300

    Update playy.py

--- YukkiMusic/plugins/play/playy.py
+++ YukkiMusic/plugins/play/playy.py
@@ -41,7 +41,7 @@ from YukkiMusic.utils.stream.stream import stream
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
 @app.on_message(
-    command(["قناه","قناة","شغل","تشغيل"])
+    command(["قناه","قناة","تشغيل-قناه","تشغيل-قناة"])
     & filters.channel
     & ~filters.edited
     & ~BANNED_USERS
