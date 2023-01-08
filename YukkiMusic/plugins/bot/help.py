a33fcbb9492811abc9acac8ed6a7a0d902e60a06
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-01-08 10:22:05 +0300

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -128,3 +128,7 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(
             helpers.HELP_6, reply_markup=keyboard
         )
+    elif cb == "hb8":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_8, reply_markup=keyboard
+        )
