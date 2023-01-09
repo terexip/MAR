ff383e7ebd37c4a1ea810e9014722987925f97bc
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-01-09 09:02:56 +0300

    Update active.py

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -24,7 +24,7 @@ ACTIVEVIDEO_COMMAND = get_command("ACTIVEVIDEO_COMMAND")
 @app.on_message(filters.command(ACTIVEVC_COMMAND) & SUDOERS)
 async def activevc(_, message: Message):
     mystic = await message.reply_text(
-        "Getting active voice chats.. Please hold"
+        "جارِ الحصول على المعلومات.. انتظر قليلاً"
     )
     served_chats = await get_active_chats()
     text = ""
@@ -33,7 +33,7 @@ async def activevc(_, message: Message):
         try:
             title = (await app.get_chat(x)).title
         except Exception:
-            title = "Private Group"
+            title = "المجموعه خاصه"
         if (await app.get_chat(x)).username:
             user = (await app.get_chat(x)).username
             text += f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})[`{x}`]\n"
@@ -44,7 +44,7 @@ async def activevc(_, message: Message):
         await mystic.edit_text("No Active Voice Chats")
     else:
         await mystic.edit_text(
-            f"**Active Voice Chats:-**\n\n{text}",
+            f"**المجموعات التي قامت بتشغيل الموسيقى:-**\n\n{text}",
             disable_web_page_preview=True,
         )
 
@@ -52,7 +52,7 @@ async def activevc(_, message: Message):
 @app.on_message(filters.command(ACTIVEVIDEO_COMMAND) & SUDOERS)
 async def activevi_(_, message: Message):
     mystic = await message.reply_text(
-        "Getting active video chats.. Please hold"
+        "جارِ الحصول على المعلومات.. انتظر قليلاً"
     )
     served_chats = await get_active_video_chats()
     text = ""
@@ -61,7 +61,7 @@ async def activevi_(_, message: Message):
         try:
             title = (await app.get_chat(x)).title
         except Exception:
-            title = "Private Group"
+            title = "المجموعه خاصه"
         if (await app.get_chat(x)).username:
             user = (await app.get_chat(x)).username
             text += f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})[`{x}`]\n"
@@ -72,6 +72,6 @@ async def activevi_(_, message: Message):
         await mystic.edit_text("No Active Voice Chats")
     else:
         await mystic.edit_text(
-            f"**Active Video Calls:-**\n\n{text}",
+            f"**المجموعات التي قامت بتشغيل الفيديو:-**\n\n{text}",
             disable_web_page_preview=True,
         )
