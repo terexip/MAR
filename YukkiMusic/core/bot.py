22d2eaaf30d13c980e2c6944f1f1a5520d4721c2
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-03-24 01:09:38 +0300

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -33,7 +33,7 @@ class YukkiBot(Client):
         self.id = get_me.id
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, "بوت الموسيقى جاهز"
+                config.LOG_GROUP_ID, "تم تشغيل البوت"
             )
         except:
             LOGGER(__name__).error(
