f64e4aae08928e12d57e336d94e20738e1e243e2
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-04-14 20:21:23 +0300

    Update yafa.py

--- YukkiMusic/plugins/tools/yafa.py
+++ YukkiMusic/plugins/tools/yafa.py
@@ -127,18 +127,3 @@ async def brah(client, message):
 @app.on_message(filters.voice_chat_ended)
 async def brah2(client, message):
        await message.reply("تم انهاء المحادثه الصوتيه.")
-       
-@app.on_message(filters.voice_chat_members_invited)
-async def fuckoff(client, message):
-           text = f"قام : {message.from_user.mention}\nبدعوة : "
-           x = 0
-           for user in message.voice_chat_members_invited.users:
-             try:
-               text += f"{user.mention} "
-               x += 1
-             except Exception:
-               pass
-           try:
-             await message.reply(f"{text} .")
-           except:
-             pass
