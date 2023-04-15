6c2700ce51b6e67e30b4e412efb2113bea88601c
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-04-15 22:09:56 +0300

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -83,6 +83,8 @@ async def play_commnd(
     url,
     fplay,
 ):
+    if not await check_is_joined(message):
+        return
     mystic = await message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
     )
