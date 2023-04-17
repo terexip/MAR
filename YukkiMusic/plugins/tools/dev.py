3b7b686b7a05af358031b8d92bb21f52599148be
Huseenytiq <118710768+SONIC-MUSIC-BOT@users.noreply.github.com>
2023-04-17 17:06:06 +0300

    Update dev.py

--- YukkiMusic/plugins/tools/dev.py
+++ YukkiMusic/plugins/tools/dev.py
@@ -23,7 +23,7 @@ async def yas(client, message):
     usr = await client.get_chat("Huseenytiq")
     name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"** 𝙎𝙊𝙉𝙄𝘾 • 𝙎𝙊𝙐𝙍𝘾𝙀 \n\n‍ ¦المطور :{name}\n ¦معرف المطور  :@{usr.username}\n ¦ايدي المطور  :`{usr.id}`\n ¦بايو المطور  :{usr.bio}\n\n**  𝙎𝙊𝙉𝙄𝘾 • 𝙎𝙊𝙐𝙍𝘾𝙀 **", 
+    await message.reply_photo(photo,       caption=f"** - 𝒎𝒂𝒊𝒏 𝒅𝒆𝒗𝒆𝒍𝒐𝒑𝒆𝒓 𝒊𝒏𝒇𝒐𝒓𝒎𝒂𝒕𝒊𝒐𝒏 : \n\n‍ ⌔︙اسم المطور \n :{name}\n ⌔︙معرف المطور \n :@{usr.username}\n ⌔︙ايدي المطور \n :`{usr.id}`\n ⌔︙بايو المطور \n :{usr.bio}\n\n****", 
     reply_markup=InlineKeyboardMarkup(
             [
                 [
